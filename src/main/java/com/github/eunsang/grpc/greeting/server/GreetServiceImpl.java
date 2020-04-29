7192e35a19b532014013024a8d8fef908c9c586f
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-04-29 06:06:05 +0000

    greeting example

---
+++ src/main/java/com/github/eunsang/grpc/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,10 @@
+package com.github.eunsang.grpc.greeting.server;
+
+import com.proto.greet.GreetServiceGrpc;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    public static void main(String[] args) {
+
+    }
+}
