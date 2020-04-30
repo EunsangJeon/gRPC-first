847afb362cab98d6e7384cbc35c649f1891f5873
Eunsang <nicolaus1207@gmail.com>
2020-04-30 21:20:29 +0900

    server streaming server

--- src/main/java/com/github/eunsang/grpc/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/eunsang/grpc/greeting/server/GreetServiceImpl.java
@@ -20,4 +20,26 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
 
         responseObserver.onCompleted();
     }
+
+    @Override
+    public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
+        String firstName = request.getGreeting().getFirstName();
+        String lastName = request.getGreeting().getFirstName();
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                String result = "Hello " + firstName + " " + lastName + ", response #: " + i;
+                GreetManyTimesResponse greetManyTimesResponse = GreetManyTimesResponse.newBuilder()
+                        .setResult(result)
+                        .build();
+
+                responseObserver.onNext(greetManyTimesResponse);
+                Thread.sleep(1000L);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            responseObserver.onCompleted();
+        }
+    }
 }
